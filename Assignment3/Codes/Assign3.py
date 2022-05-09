537217c959a9621c78444dc3d947b0a9d3f356d5
pradeep <pradeepmundlik@gmail.com>
2022-05-09 16:47:20 +0530

    Updated

--- Assignment3/Codes/Assign3.py
+++ Assignment3/Codes/Assign3.py
@@ -5,11 +5,11 @@ from sympy import false
 def f(x,y):
     return x*y
 
-A = pd.read_excel(r'C:\\Users\\91704\\Documents\\IITH PDFS\\Probability\\Assignment3\\Tables\\input.xlsx','Sheet1')
+A = pd.read_excel(r'C:\Users\91704\Documents\IITH PDFS\AI1110\Assignment3\Tables\input.xlsx','Sheet1')
 B = np.array(pd.DataFrame(A,columns=['Salary (in Rs.)']))
 C = np.array(pd.DataFrame(A,columns=['Number of Workers']))
 D = pd.DataFrame(f(B,C))
-D.to_excel(excel_writer='C:\\Users\\91704\\Documents\\IITH PDFS\\Probability\\Assignment3\\Tables\\output.xlsx',header=false, index=false)
+D.to_excel(excel_writer=r'C:\Users\91704\Documents\IITH PDFS\AI1110\Assignment3\Tables\output.xlsx',header=false, index=false)
 sum_freq = float(np.sum(C))
 sum_salary = float(np.sum(D))
 print("Mean salary of workers is: ",sum_salary/sum_freq)
