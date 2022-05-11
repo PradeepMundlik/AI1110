203f6aaf8dfebd56fb63eb39cffc8163d1f8b2f0
pradeep <pradeepmundlik@gmail.com>
2022-05-11 09:26:21 +0530

    Updated

--- Assignment5/Codes/assign5.py
+++ Assignment5/Codes/assign5.py
@@ -2,7 +2,7 @@
 #P(B) = 0.4
 P_A = 0.3
 P_B = 0.4
-print(f"P(A intersection B) = {P_A*P_B}")
-print(f"P(A union B) = {P_A + P_B - P_A*P_B}")
+print(f"P(AB) = {P_A*P_B}")
+print(f"P(A+B) = {P_A + P_B - P_A*P_B}")
 print(f"P(A|B) = {P_A}")
 print(f"P(B|A) = {P_B}")
